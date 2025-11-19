52070bb866af5394902c28513c7c02b6e313854f
prtyksh <flimsy21@protonmail.com>
2025-11-19 05:21:09 +0000

    Added User Class, AuthService and a LoginScreen along with putting it all together with Main.java

--- src/main/java/edu/univ/erp/Main.java
+++ src/main/java/edu/univ/erp/Main.java
@@ -1,18 +1,24 @@
 package edu.univ.erp;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import edu.univ.erp.ui.LoginScreen;
+import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and Welcome to us!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // FlatLaf Setup
+        try{
+            FlatLightLaf.setup();
         }
+        catch (Exception e){
+            System.out.println("Couldn't Setup FlatLaf");
+        }
+
+        // Launch the UI
+        SwingUtilities.invokeLater(() -> {
+           LoginScreen login = new LoginScreen();
+           login.setVisible(true);
+        });
     }
 } 
