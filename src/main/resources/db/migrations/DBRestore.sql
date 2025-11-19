34290483fabb336a6d42b52a583fe4eb79b6f9e6
prtyksh <flimsy21@protonmail.com>
2025-11-19 03:20:13 +0000

    Added the DB Logic

---
+++ src/main/resources/db/migrations/DBRestore.sql
@@ -0,0 +1,74 @@
+-- Cleanup and Restore the DB on each of our machines
+
+
+SET FOREIGN_KEY_CHECKS = 0;
+    TRUNCATE TABLE authDB.users;
+    TRUNCATE TABLE erpDB.students;
+    TRUNCATE TABLE erpDB.instructors;
+    TRUNCATE TABLE erpDB.grades;
+    TRUNCATE TABLE erpDB.enrollments;
+    TRUNCATE TABLE erpDB.sections;
+    TRUNCATE TABLE erpDB.courses;
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+-- Dummy Data For AuthDB
+    -- user Table
+    INSERT INTO  authDB.users (userID, userName, role, passwdHash, status) VALUES
+                                                                               (1, "admin", "ADMIN", "$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRryNBIAdUWZIXMAVbnBh.9WMtq", "ACTIVE"),
+                                                                               (2, "newton", "INSTRUCTOR", "$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRryNBIAdUWZIXMAVbnBh.9WMtq", "ACTIVE"),
+                                                                               (3, "turing", "INSTRUCTOR", "$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRryNBIAdUWZIXMAVbnBh.9WMtq", "ACTIVE"),
+                                                                               (4, "guevara", "STUDENT", "$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRryNBIAdUWZIXMAVbnBh.9WMtq", "ACTIVE"),
+                                                                               (5, "dostoyevesky", "STUDENT", "$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRryNBIAdUWZIXMAVbnBh.9WMtq", "ACTIVE"),
+                                                                               (6, "wilde", "STUDENT", "$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRryNBIAdUWZIXMAVbnBh.9WMtq", "INACTIVE")
+    ;
+
+-- Dummy Data for erpDB
+    -- instructors Table
+    INSERT INTO erpDB.instructors (userID, fullName, department) VALUES
+                                                                     (2, "Issac Newton", "Physics"),
+                                                                     (3, "Alan Turing", "Computer Science")
+    ;
+
+    -- Students Table
+    INSERT INTO erpDB.students (userID, roll, firstName, lastName, program, passOutYear) VALUES
+                                                                                             (4,"2024501", "Ernesto", "Guevara", "B.Tech CSSS", 2028),
+                                                                                             (5, "2024502", "Fyodor", "Dostoyevesky", "B.Tech CSE.Econ", 2028),
+                                                                                             (6, "2024503", "Oscar", "Wilde","B.Tech CSB", 2028)
+    ;
+
+    -- Courses Table
+        INSERT INTO erpDB.courses( courseID, courseCode, courseName, credits) VALUES
+                                                                                  (101, 'CSE101', 'Intro To Programming', 4),
+                                                                                  (102, 'CSE201', 'Data Structures and Algorithms', 4),
+                                                                                  (103, 'PHY101', 'Quantum Mechanics', 3),
+                                                                                  (104, 'MTH101', 'Calculus I', 4)
+
+            ;
+
+
+    -- Sections Table
+    INSERT INTO erpDB.sections (sectionID, courseID, instructorID, term, room, schedule, capacity) VALUES
+                                                                                                       (1, 101, 3, "Fall 2025", "C-101", "Mon 0900 - 1100 Hours", 60),
+                                                                                                       (2, 102, 3, "Fall 2025", "L1", "Tue 1100-1300 Hrs", 1),
+                                                                                                       (3, 103, 2, "Fall 2025", "C-201", "Wed 1300-1400 Hrs", 60)
+    ;
+
+    -- Enrolllments Table
+    INSERT INTO erpDB.enrollments (enrollmentID, studentID, sectionID, status) VALUES
+                                                                                   (1,4,1, "ENROLLED"),
+                                                                                   (2,4,3, "ENROLLED"),
+                                                                                   (3,5,1, "ENROLLED")
+    ;
+
+    -- Grades Table
+    INSERT INTO erpDB.grades (enrollmentID, componentName, score, maxScore) Values
+                                                                                (1, "Mid Sem", 85.55, 90.00),
+                                                                                (1, "Assignment 1", 18.00, 20.00),
+                                                                                (3, "Mid Sem", 75.00, 90.00)
+    ;
+
+
+
+
